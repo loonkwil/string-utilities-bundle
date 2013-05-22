713192291f3a6c57a8c196190cfc2e8c31bc807b
Peter Simon <simonpepe@gmail.com>
2013-05-22 10:32:20 +0200

    Refactoring tests

--- Tests/StringUtilitiesTest.php
+++ Tests/StringUtilitiesTest.php
@@ -6,60 +6,69 @@ use SPE\StringUtilitiesBundle\StringUtilities;
 
 class StringUtilitiesTest extends \PHPUnit_Framework_TestCase
 {
+    protected $st;
+
+    protected function setUp()
+    {
+        $this->st = new StringUtilities();
+    }
+
+    protected function checkSlug($slug, $str)
+    {
+        $this->assertEquals($slug, $this->st->slugify($str));
+    }
+
+
     public function testRandomString()
     {
-        $st = new StringUtilities;
-
         // 10 hosszu
-        $str = $st->getRandomString(10);
+        $str = $this->st->getRandomString(10);
         $this->assertEquals(10, strlen($str));
 
         // csak kis betuk
-        $str = $st->getRandomString(19, false, false);
+        $str = $this->st->getRandomString(19, false, false);
         $this->assertGreaterThan(0, preg_match('/^[a-z]{19}$/', $str));
 
         // csak kis egy nagy betuk
-        $str = $st->getRandomString(18, false, true);
+        $str = $this->st->getRandomString(18, false, true);
         $this->assertGreaterThan(0, preg_match('/^[a-zA-Z]{18}$/', $str));
 
         // kis, nagy betuk, szamok
-        $str = $st->getRandomString(17, true, true);
+        $str = $this->st->getRandomString(17, true, true);
         $this->assertGreaterThan(0, preg_match('/^[a-zA-Z0-9]{17}$/', $str));
 
         // kis betuk es szamok
-        $str = $st->getRandomString(16, true, false);
+        $str = $this->st->getRandomString(16, true, false);
         $this->assertGreaterThan(0, preg_match('/^[a-z0-9]{16}$/', $str));
     }
 
     public function testSlugify()
     {
-        $st = new StringUtilities;
-
         // converts all characters to lower case
-        $this->assertEquals('sensio', $st->slugify('Sensio'));
+        $this->checkSlug('sensio', 'Sensio');
 
         // replaces a white space by a -
-        $this->assertEquals('sensio-labs', $st->slugify('sensio-labs'));
+        $this->checkSlug('sensio-labs', 'sensio labs');
 
         // replaces several white spaces by a single -
-        $this->assertEquals('sensio-labs', $st->slugify('sensio   labs'));
+        $this->checkSlug('sensio-labs', 'sensio   labs');
 
         // removes - at the beginning of a string
-        $this->assertEquals('sensio', $st->slugify('  sensio'));
+        $this->checkSlug('sensio', '  sensio');
 
         // removes - at the end of a string
-        $this->assertEquals('sensio', $st->slugify('sensio  '));
+        $this->checkSlug('sensio', 'sensio  ');
 
         // replaces non-ASCII characters by a -
-        $this->assertEquals('paris-france', $st->slugify('paris,france'));
+        $this->checkSlug('paris-france', 'paris,france');
 
         // converts the empty string to n-a
-        $this->assertEquals('n-a', $st->slugify(''));
+        $this->checkSlug('n-a', '');
 
         // removes accents
-        $this->assertEquals('developpeur-web', $st->slugify('Développeur Web'));
+        $this->checkSlug('developpeur-web', 'Développeur Web');
 
         // removes accents
-        $this->assertEquals('ouooueauiouooueaui', $st->slugify('öüóőúéáűíÖÜÓŐÚÉÁŰÍ'));
+        $this->checkSlug('ouooueauiouooueaui', 'öüóőúéáűíÖÜÓŐÚÉÁŰÍ');
     }
 }
